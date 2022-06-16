2b31c83b22bd67924ea3ce808edd7f89ea90dd3e
@anhnv99 <vietanhhappy99@gmail.com>
2022-06-16 10:02:10 +0700

    add

---
+++ src/components/AddContact/index.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router";
+import { toast } from "react-toastify";
+
+const AddPost = ({ contacts, addContact }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const checkContactEmailExists = contacts.filter((contact) =>
+      contact.email === email ? contact : null
+        //> nhập trùng giá trị email sẽ báo error => toast.error("This email already exists!!")
+    );
+    const checkContactPhoneExists = contacts.filter((contact) =>
+      contact.phone === phone ? contact : null
+        //> nhập trùng giá trị phone sẽ báo error => toast.error("This phone number already exists!!")
+    );
+
+    if (!email || !name || !phone) {
+      return toast.warning("Please fill in all fields!!");
+    }
+    if (checkContactEmailExists.length > 0) {
+      return toast.error("This email already exists!!");
+    }
+    if (checkContactPhoneExists.length > 0) {
+      return toast.error("This phone number already exists!!");
+    }
+
+    console.log("contacts[contacts.length - 1] = ",contacts[contacts.length - 1] )
+    //> {id: 1, name: 'hello', email: 'testgmail@gmail.com', phone: 4567891230}
+    console.log( " contacts id:" ,contacts[contacts.length - 1].id + 1) //>  contacts id: 2
+
+    const data = {
+      id: contacts.length > 0 ? contacts[contacts.length - 1].id + 1 : 0,
+      email,
+      name,
+      phone,
+    };
+    console.log("data",data)
+
+    addContact(data);
+    toast.success("Contact added successfully!!");
+    history.push("/");
+  };
+
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center text-dark py-3 display-2">Add Post</h1>
+      <div className="row">
+        <div className="col-md-6 p-5 mx-auto shadow">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Full name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="number"
+                placeholder="Phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="btn btn-block btn-dark"
+                type="submit"
+                value="Add Student"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  contacts: state,
+});
+const mapDispatchToProps = (dispatch) => ({
+  addContact: (data) => {
+    dispatch({ type: "ADD_CONTACT", payload: data });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddPost);
---
+++ src/redux/reducers/contactReducer.js
@@ -0,0 +1,35 @@
+const initialState = [
+  { id: 0, name: "Viet Anh", email: "vietanhhappy@gmail.com", phone: 1234567890 },
+  { id: 1, name: "hello", email: "testgmail@gmail.com", phone: 4567891230 },
+];
+
+export const contactReducer = (state = initialState, action) => {
+  console.log('--------------',action)
+  //> {type: 'ADD_CONTACT', payload: {…}}
+  console.log('--------------',action.payload)      
+  //> {id: 2, email: 'test@gmail.com', name: 'hello', phone: '0999999988888888'}
+  switch (action.type) {
+    case "ADD_CONTACT":
+      state = [...state, action.payload];
+      return state;
+    case "DELETE_CONTACT":
+      const contactFilter = state.filter((contact) =>
+        contact.id === action.payload ? null : contact
+      );
+      state = contactFilter;
+      return state;
+    case "UPDATE_CONTACT":
+      const contactUpdate = state.filter((contact) =>
+        contact.id === action.payload.id
+          ? Object.assign(contact, action.payload)
+          : contact
+      );
+      state = contactUpdate;
+      return state;
+    case "RESET_CONTACT":
+      state = [{ name: null, email: null, phone: null }];
+      return state;
+    default:
+      return state;
+  }
+};
